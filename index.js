01884c5ea0025051becbdda835c35c23c7a847b0
GermanKudinov <113067122+GermanKudinov@users.noreply.github.com>
2023-02-09 21:43:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const TelegramBot = require("node-telegram-bot-api");
+const { url } = require("inspector");
+const TOKEN = "6210361489:AAHxDakg0fTKF95YhgXlz2x2C9knOiAJ0bM";
+const server = express();
+const bot = new TelegramBot(TOKEN, {
+    polling: true
+});
+const port = process.env.PORT || 5000;
+const gameName = "GAMEFORDOWN";
+const queries = {};
+server.use(express.static(path.join(__dirname, 'tipo bot')));
+bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This game for down. Say /game if you want to play."));
+bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
+bot.on("callback_query", function (query) {
+    if (query.game_short_name !== gameName) {
+        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+    } else {
+        queries[query.id] = query;
+        let gameurl = "https://maxizhukov.github.io/telegram_game_front/";
+        bot.answerCallbackQuery({
+            callback_query_id: query.id,
+            url: gameurl
+        });
+        // bot.on('callback_query', (query) => {
+        //     bot.answerCallbackQuery({
+        //              callback_query_id: query.id,
+        //              url: gameurl
+        //         });
+        // });
+    }
+});
+bot.on("inline_query", function (iq) {
+    bot.answerInlineQuery(iq.id, [{
+        type: "game",
+        id: "0",
+        game_short_name: gameName
+    }]);
+});
+server.get("/highscore/:score", function (req, res, next) {
+    if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
+    let query = queries[req.query.id];
+    let options;
+    if (query.message) {
+        options = {
+            chat_id: query.message.chat.id,
+            message_id: query.message.message_id
+        };
+    } else {
+        options = {
+            inline_message_id: query.inline_message_id
+        };
+    }
+    bot.setGameScore(query.from.id, parseInt(req.params.score), options,
+        function (err, result) {});
+});
+server.listen(port);
